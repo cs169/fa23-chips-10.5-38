ee7d3883d0fc64ca42822a017f3c1359536dd304
Amanda Sayaseng <amandasayaseng@berkeley.edu>
2023-12-01 07:24:44 +0000

    started iteration 2 part 1, but not sure if doing correctly

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -9,4 +9,33 @@ class NewsItem < ApplicationRecord
       representative_id: representative_id
     )
   end
+
+  # Add the 'Issue' column to the NewsItem model
+  enum issue: {
+    "Free Speech": "Free Speech",
+    "Immigration": "Immigration",
+    "Terrorism": "Terrorism",
+    "Social Security and Medicare": "Social Security and Medicare",
+    "Abortion": "Abortion",
+    "Student Loans": "Student Loans",
+    "Gun Control": "Gun Control",
+    "Unemployment": "Unemployment",
+    "Climate Change": "Climate Change",
+    "Homelessness": "Homelessness",
+    "Racism": "Racism",
+    "Tax Reform": "Tax Reform",
+    "Net Neutrality": "Net Neutrality",
+    "Religious Freedom": "Religious Freedom",
+    "Border Security": "Border Security",
+    "Minimum Wage": "Minimum Wage",
+    "Equal Pay": "Equal Pay"
+  }
+  # Method to create a news item with the selected issue
+  def self.create_with_issue(representative_id, issue)
+    NewsItem.create!(
+      representative_id: representative_id,
+      issue: issue
+    )
+    end
+  
 end
