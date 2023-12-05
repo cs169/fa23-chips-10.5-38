e61060850968e90295f43b3b0eb1fa35bf162dd8
Jackson Xu <jacksonxu@berkeley.edu>
2023-12-05 22:36:41 +0000

    style fix 3

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -8,28 +8,19 @@ RSpec.describe Representative, type: :model do
   describe '.civic_api_to_representative_params' do
     it 'returns an array of representatives with valid input' do
       # Simulating civic API response
-      rep_info = instance_double(RepInfo,
-                                 officials: [
-                                   instance_double(Official, {
-                                                     name:    'name1',
-                                                     title:   "Mayor's Office",
-                                                     party:   'Republican',
-                                                     address: [instance_double(Address, {
-                                                                                 line1: '123 Main St',
-                                                                                 city:  'Cityville',
-                                                                                 zip:   '54321',
-                                                                                 state: 'CA'
-                                                                               })]
-                                                   })
-                                 ],
-                                 offices:   [
-                                   instance_double(Office, {
-                                                     official_indices: [0],
-                                                     name:             "Mayor's Office",
-                                                     division_id:      'ocdid123'
-                                                   })
-                                 ])
+      rep_info = double('rep_info',
+                        officials: [
+                          double('official1', name: 'name1', title: "Mayor's Office", party: 'Republican',
+address: [double('address1', line1: '123 Main St', city: 'Cityville', zip: '54321', state: 'CA')]),
+                          double('official2', name: 'name2', title: 'Council Office', party: 'Democrat',
+address: [double('address2', line1: '456 Oak St', city: 'Townsville', zip: '98765', state: 'NY')])
+                        ],
+                        offices:   [
+                          double('office1', official_indices: [0], name: "Mayor's Office", division_id: 'ocdid123'),
+                          double('office2', official_indices: [1], name: 'Council Office', division_id: 'ocdid456')
+                        ])
       allow(rep_info.officials[0]).to receive(:photo_url).and_return('http://example.com/photo1.jpg')
+      allow(rep_info.officials[1]).to receive(:photo_url).and_return('http://example.com/photo2.jpg')
 
       # Mock the 'offices' method to return the expected array
       allow(rep_info).to receive(:offices).and_return(rep_info.offices)
@@ -37,7 +28,7 @@ RSpec.describe Representative, type: :model do
       representatives = described_class.civic_api_to_representative_params(rep_info)
 
       expect(representatives).to be_an(Array)
-      expect(representatives.length).to eq(1)
+      expect(representatives.length).to eq(2)
 
       expect(representatives[0].name).to eq('name1')
       expect(representatives[0].title).to eq("Mayor's Office")
@@ -46,30 +37,33 @@ RSpec.describe Representative, type: :model do
       expect(representatives[0].zip).to eq('54321')
       expect(representatives[0].state).to eq('CA')
       expect(representatives[0].party).to eq('Republican')
+
+      expect(representatives[1].name).to eq('name2')
+      expect(representatives[1].title).to eq('Council Office')
+      expect(representatives[1].street).to eq('456 Oak St')
+      expect(representatives[1].city).to eq('Townsville')
+      expect(representatives[1].zip).to eq('98765')
+      expect(representatives[1].state).to eq('NY')
+      expect(representatives[1].party).to eq('Democrat')
     end
   end
 
   describe '.asign_address' do
     it 'creates or updates a representative with valid input' do
       # Simulating civic API response
-      rep_info = instance_double(RepInfo, {
-                                   address:   [instance_double(Address, {
-                                                                 line1: '123 Main St',
-                                                                 city:  'Cityville',
-                                                                 zip:   '54321',
-                                                                 state: 'CA'
-                                                               })],
-                                   name:      'name1',
-                                   party:     'Republican',
-                                   photo_url: 'http://example.com/photo.jpg'
-                                 })
+      rep_info = double('rep_info',
+                        address:   [double('address', line1: '123 Main St', city: 'Cityville', zip: '54321',
+state: 'CA')],
+                        name:      'name1',
+                        party:     'Republican',
+                        photo_url: 'http://example.com/photo.jpg')
       allow(rep_info).to receive(:offices).and_return([
-                                                        instance_double(Office, {
-                                                                          official_indices: [0],
-                                                                          name:             "Mayor's Office",
-                                                                          division_id:      'ocdid123'
-                                                                        })
+                                                        double('office1', official_indices: [0],
+name: "Mayor's Office", division_id: 'ocdid123')
                                                       ])
+
+      title_temp = "Mayor's Office"
+      ocdid_temp = 'ocdid123'
 
       representative = described_class.asign_address(rep_info, rep_info, 0)
 
@@ -88,20 +82,11 @@ RSpec.describe Representative, type: :model do
   describe '.get_office' do
     it 'returns title and ocdid with valid input' do
       # Simulating civic API response
-      rep_info = instance_double(RepInfo, {
-                                   offices: [
-                                     instance_double(Office, {
-                                                       official_indices: [0],
-                                                       name:             "Mayor's Office",
-                                                       division_id:      'ocdid123'
-                                                     }),
-                                     instance_double(Office, {
-                                                       official_indices: [1],
-                                                       name:             'Council Office',
-                                                       division_id:      'ocdid456'
-                                                     })
-                                   ]
-                                 })
+      rep_info = double('rep_info',
+                        offices: [
+                          double('office1', official_indices: [0], name: "Mayor's Office", division_id: 'ocdid123'),
+                          double('office2', official_indices: [1], name: 'Council Office', division_id: 'ocdid456')
+                        ])
 
       title, ocdid = described_class.get_office(rep_info, 0)
 
